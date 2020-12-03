9f60ea54034f6d6efb99e05cd05d0e4b23372ecc
Marta <martapabis.it@gmail.com>
2020-12-03 12:28:15 +0100

    add a file

---
+++ asp.net/projekt/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projekt.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        [Display(Name = "Nazwa")]
+        public string Name { get; set; }
+        
+        [Display(Name = "Opis")]
+        public string Description { get; set; }
+        
+        [Range(0.01, double.MaxValue, ErrorMessage = "Proszę dodać dodatnią cenę.")]
+        [Display(Name = "Cena label")]
+        public Decimal Price { get; set; }
+        
+        [Display(Name = "Kategoria")]
+        public string Category { get; set; }
+    }
+}
